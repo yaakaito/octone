60b584536385114cbb627dd96dcd496185e704d8
KAZUMA Ukyo <yaakaito@gmail.com>
2012-08-23 13:55:06 +0900

    ghunit

---
+++ BrowseGithubTests/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BrowseGithubTests
+//
+//  Created by KAZUMA Ukyo on 12/08/23.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "GHUnitIOSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    }
+}
