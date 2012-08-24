cf7e8716924a5b1bf1013962e3a99eec258046d3
KAZUMA Ukyo <yaakaito@gmail.com>
2012-08-24 14:21:03 +0900

    touch global menu spec

---
+++ BrowseGithubSpecs/BGGlobalMenuControllerSpec.m
@@ -0,0 +1,32 @@
+//
+//  BrowseGithub - BGGlobalMenuControllerSpec.m
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+//  Created by: KAZUMA Ukyo
+//
+
+#import "Kiwi.h"
+#import "BGGlobalMenuControllerSpec.m"
+
+SPEC_BEGIN(BGGlobalMenuControllerSpec)
+
+describe(@"BGGlobalMenuController", ^{
+    __block NSString *string;
+    context(@"New", ^{
+        beforeEach(^{
+            string = @"example";
+        });
+        
+        context(@"append 'exsample'", ^{
+            beforeEach(^{
+                string = [string stringByAppendingString:@"example"];
+            });
+            
+            it(@"length = 14", ^{
+                [[theValue([string length]) should] equal:theValue(15)];
+            });
+        });
+    });
+});
+
+SPEC_END
