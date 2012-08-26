d7b0654922ce48e6734814a776d6e9a896aaa21d
KAZUMA Ukyo <yaakaito@gmail.com>
2012-08-27 06:14:53 +0900

    ユーザー表示用のビュー

---
+++ BrowseGithub/BGGlobalMenuUserView.h
@@ -0,0 +1,13 @@
+//
+//  BGGlobalMenuUserView.h
+//  BrowseGithub
+//
+//  Created by KAZUMA Ukyo on 12/08/24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BGGlobalMenuUserView : UIView
+
+@end
---
+++ BrowseGithub/BGGlobalMenuUserView.m
@@ -0,0 +1,32 @@
+//
+//  BGGlobalMenuUserView.m
+//  BrowseGithub
+//
+//  Created by KAZUMA Ukyo on 12/08/24.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "BGGlobalMenuUserView.h"
+
+@implementation BGGlobalMenuUserView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+        self.backgroundColor = [UIColor blackColor];
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
