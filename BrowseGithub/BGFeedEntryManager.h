9fbd2e0614bf2fa6e61ad85ad3b9c0457faf396a
KAZUMA Ukyo <yaakaito@gmail.com>
2012-08-29 03:52:16 +0900

    touch feed manager

---
+++ BrowseGithub/BGFeedEntryManager.h
@@ -0,0 +1,13 @@
+//
+//  BGFeedEntryManager.h
+//  BrowseGithub
+//
+//  Created by yaakaito on 12/08/28.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BGFeedEntryManager : NSObject
+
+@end
---
+++ BrowseGithub/BGFeedEntryManager.m
@@ -0,0 +1,13 @@
+//
+//  BGFeedEntryManager.m
+//  BrowseGithub
+//
+//  Created by yaakaito on 12/08/28.
+//
+//
+
+#import "BGFeedEntryManager.h"
+
+@implementation BGFeedEntryManager
+
+@end
