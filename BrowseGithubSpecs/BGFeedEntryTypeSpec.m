181602bd30f970243231f363dd90876f25e8d3c0
KAZUMA Ukyo <yaakaito@gmail.com>
2012-08-31 10:22:02 +0900

    typeのアサーションをヘルパー関数を使った物に変更した

--- BrowseGithubSpecs/BGFeedEntryTypeSpec.m
+++ BrowseGithubSpecs/BGFeedEntryTypeSpec.m
@@ -13,98 +13,83 @@ SPEC_BEGIN(BGFeedEntryTypeSpec)
 describe(@"Feed Entry", ^{
 
     context(@"typeのパース", ^{
-        __block BGFeedEntry *feedEntry;
-        beforeEach(^{
-            feedEntry = [[BGFeedEntry alloc] init];
-        });
-        
         it(@"初期化状態ではidがセットされていないので分からない", ^{
+            BGFeedEntry *feedEntry = [[BGFeedEntry alloc] init];
             [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeUnknown)];
         });
+
+        void(^checkFeedType)(BGFeedEntryType, NSString*) = ^(BGFeedEntryType type, NSString* entryId) {
+            BGFeedEntry *feedEntry = [[BGFeedEntry alloc] init];
+            feedEntry.entryId = entryId;
+            [[theValue(feedEntry.type) should] equal:theValue(type)];
+        };
         
         it(@"idにCommitが含まれるとき、typeはCommit", ^{
-            feedEntry.entryId = @"tag:github.com,2008:CommitEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeCommit, @"tag:github.com,2008:CommitEvent/1000000");
         });
         
         it(@"idにPushが含まれるとき、typeはPush", ^{
-            feedEntry.entryId = @"tag:github.com,2008:PushEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypePush,  @"tag:github.com,2008:PushEvent/1000000");
         });
         
         it(@"idにForkAppleが含まれるとき、typeはForkApply", ^{
-            feedEntry.entryId = @"tag:github.com,2008:ForkApplyEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeForkApply, @"tag:github.com,2008:ForkApplyEvent/1000000");
         });
         
         it(@"idにForkが含まれるとき、typeはFork", ^{
-            feedEntry.entryId = @"tag:github.com,2008:ForkEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeFork,  @"tag:github.com,2008:ForkEvent/1000000");
         });
         
         it(@"idにPullRequestが含まれるとき、typeはPullRequest", ^{
-            feedEntry.entryId = @"tag:github.com,2008:PullRequestEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypePullRequest, @"tag:github.com,2008:PullRequestEvent/1000000");
         });
         
         it(@"idにIssueが含まれるとき、typeはIssue", ^{
-            feedEntry.entryId = @"tag:github.com,2008:IssueEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeIssue, @"tag:github.com,2008:IssueEvent/1000000");
         });
         
         it(@"idにFollowが含まれるとき、typeはFollow", ^{
-            feedEntry.entryId = @"tag:github.com,2008:Follow/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeFollow, @"tag:github.com,2008:Follow/1000000");
         });
         
         it(@"idにWatchが含まれるとき、typeはWatch", ^{
-            feedEntry.entryId = @"tag:github.com,2008:WatchEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeWatch, @"tag:github.com,2008:WatchEvent/1000000");
         });
         
         it(@"idにStarが含まれるとき、typeはStar", ^{
-            feedEntry.entryId = @"tag:github.com,2008:StarEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeStar, @"tag:github.com,2008:StarEvent/1000000");
         });
         
         it(@"idにCommitCommentが含まれるとき、typeはCommitComment", ^{
-            feedEntry.entryId = @"tag:github.com,2008:CommitCommentEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeCommitComment, @"tag:github.com,2008:CommitCommentEvent/1000000");
         });
         
         it(@"idにIssueCommentが含まれるとき、typeはIssueComment", ^{
-            feedEntry.entryId = @"tag:github.com,2008:IssueComment/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeIssueComment, @"tag:github.com,2008:IssueComment/1000000");
         });
         
         it(@"idにCreateが含まれるとき、typeはCreate", ^{
-            feedEntry.entryId = @"tag:github.com,2008:CreateEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeCreate, @"tag:github.com,2008:CreateEvent/1000000");
         });
         
         it(@"idにDeleteが含まれるとき、typeはDelete", ^{
-            feedEntry.entryId = @"tag:github.com,2008:DeleteEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeDelete, @"tag:github.com,2008:DeleteEvent/1000000");
         });
         
         it(@"idにWikiが含まれるとき、typeはWiki", ^{
-            feedEntry.entryId = @"tag:github.com,2008:WikiEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeWiki, @"tag:github.com,2008:WikiEvent/1000000");
         });
         
         it(@"idにDownloadが含まれるとき、typeはDownload", ^{
-            feedEntry.entryId = @"tag:github.com,2008:DownloadEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeDownload, @"tag:github.com,2008:DownloadEvent/1000000");
         });
         
         it(@"idにTeamAddが含まれるとき、typeはTeamAdd", ^{
-            feedEntry.entryId = @"tag:github.com,2008:TeamAddEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeTeamAdd, @"tag:github.com,2008:TeamAddEvent/1000000");
         });
         
         it(@"idにMemberが含まれるとき、typeはMember", ^{
-            feedEntry.entryId = @"tag:github.com,2008:MemberEvent/1000000";
-            [[theValue(feedEntry.type) should] equal:theValue(kBGFeedEntryTypeCommit)];
+            checkFeedType(kBGFeedEntryTypeMember, @"tag:github.com,2008:MemberEvent/1000000");
         });
     });
 });
