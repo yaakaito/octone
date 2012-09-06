428f65edf0df36b17dc5c7accb631ffada1f5d69
KAZUMA Ukyo <yaakaito@gmail.com>
2012-09-06 13:14:14 +0900

    fix https

--- BrowseGithubSpecs/BGGithubResourceSpec.m
+++ BrowseGithubSpecs/BGGithubResourceSpec.m
@@ -18,26 +18,26 @@ describe(@"Github Resource", ^{
         it(@"は、APIパスを引数に受け取り、URLを構築する", ^{
             githubResorce = [[BGGithubResource alloc] initWithPath:@"/user"];
             NSURL *url = githubResorce.resourceUrl;
-            [[[url absoluteString] should] equal:@"http://api.github.com/user"];
+            [[[url absoluteString] should] equal:@"https://api.github.com/user"];
         });
         
         it(@"は、クエリ付きのAPIパスを引数に受け取り、URLを構築する", ^{
             githubResorce = [[BGGithubResource alloc] initWithPath:@"/user?page=1"];
             NSURL *url = githubResorce.resourceUrl;
-            [[[url absoluteString] should] equal:@"http://api.github.com/user"];
+            [[[url absoluteString] should] equal:@"https://api.github.com/user"];
         });
         
         it(@"は、APIパスを引数に受け取り、認可情報を付加したURLを構築する", ^{
             githubResorce = [[BGGithubResource alloc] initWithAuthAndPath:@"/user"];
             NSURL *url = githubResorce.resourceUrl;
-            NSString *urlString = [NSString stringWithFormat:@"http://api.github.com/user?access_token=%@", kDebugAccessToken];
+            NSString *urlString = [NSString stringWithFormat:@"https://api.github.com/user?access_token=%@", kDebugAccessToken];
             [[[url absoluteString] should] equal:urlString];
         });
         
         it(@"は、クエリ付きのAPIパスを引数に受け取り、認可情報を付加したURLを構築する", ^{
             githubResorce = [[BGGithubResource alloc] initWithAuthAndPath:@"/user?page=1"];
             NSURL *url = githubResorce.resourceUrl;
-            NSString *urlString = [NSString stringWithFormat:@"http://api.github.com/user?page=1&access_token=%@", kDebugAccessToken];
+            NSString *urlString = [NSString stringWithFormat:@"https://api.github.com/user?page=1&access_token=%@", kDebugAccessToken];
         });
     });
 });
