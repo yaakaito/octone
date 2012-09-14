796cd9a2fe117ae1595c6b75c79f83d643f46352
KAZUMA Ukyo <yaakaito@gmail.com>
2012-09-14 14:30:48 +0900

    feedparserでxml解析はしない

--- BrowseGithub/BGFeedParser.m
+++ BrowseGithub/BGFeedParser.m
@@ -7,14 +7,6 @@
 //
 
 #import "BGFeedParser.h"
-#import <KissXML/DDXML.h>
 
 @implementation BGFeedParser
-
-+ (NSArray *)parseFeedXMLData:(NSData *)xmlData error:(NSError *__autoreleasing *)error{
-    
-    DDXMLDocument *document = [[DDXMLDocument alloc] initWithData:xmlData options:0 error:error];
-    
-    return [NSArray array];
-}
 @end
