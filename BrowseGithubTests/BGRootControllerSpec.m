f8213a69346510bfd26d714b587d8c91015d0e04
KAZUMA Ukyo <yaakaito@gmail.com>
2012-09-16 00:29:41 +0900

    RootControllerで必要そうな機能を記述

--- BrowseGithubTests/BGRootControllerSpec.m
+++ BrowseGithubTests/BGRootControllerSpec.m
@@ -6,26 +6,61 @@
 //
 
 #import "Kiwi.h"
+#import "BGRootController.h"
 
 SPEC_BEGIN(BGRootControllerSpec)
 
-describe(@"Example", ^{
-    __block NSString *string;
-    context(@"New", ^{
-        beforeEach(^{
-            string = @"example";
+describe(@"Root Controller ", ^{
+    __block BGRootController *controller;
+    context(@"が初期されたとき", ^{
+
+        xit(@"は、エラー通知用のnotificationを受け取れる", ^{
+            ;
         });
         
-        context(@"append 'exsample'", ^{
-            beforeEach(^{
-                string = [string stringByAppendingString:@"example"];
+        xit(@"は、OAuth用のコントローラーを生成できる", ^{
+            
+        });
+        
+        xit(@"は、アプリケーションのコントローラーを生成できる", ^{
+            
+        });
+        
+        
+        context(@"にOAuthが行われていないとき", ^{
+            xit(@"は、OAuth用のコントローラーを起動する", ^{
+                
             });
             
-            it(@"length = 14", ^{
-                [[theValue([string length]) should] equal:theValue(15)];
+            xit(@"は、OAuthに成功したことを受け取ったら、アプリケーションのコントローラーに切り替える", ^{
+                
             });
         });
+        
+        context(@"にOAuthが行われているとき", ^{
+            xit(@"は、アプリケーションのコントローラーを起動する", ^{
+                
+            });
+        });
+
     });
+
+    
+    context(@"がエラー通知を受け取ったとき", ^{
+        
+        xit(@"は、ネットワークのエラーなら、connectionエラーを表示する", ^{
+            
+        });
+        
+        xit(@"は、APIのエラーなら、APIエラーを表示する", ^{
+            ;
+        });
+        
+        xit(@"は、OAuth関連のエラーなら、OAuthエラーを表示し、OAuth画面を表示する", ^{
+            ;
+        });
+    });
+
 });
 
 SPEC_END
