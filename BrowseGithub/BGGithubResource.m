8c9f50a10c3a9b2a6bf66176bc8cc856bc15a09f
KAZUMA Ukyo <yaakaito@gmail.com>
2012-09-15 12:23:20 +0900

    add User-Agent

--- BrowseGithub/BGGithubResource.m
+++ BrowseGithub/BGGithubResource.m
@@ -35,7 +35,8 @@
 - (void)loadDataWithComplete:(void (^)(void))complete
                      failure:(void (^)(void))failure {
     
-    NSURLRequest *request = [NSURLRequest requestWithURL:self.resourceUrl];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:self.resourceUrl];
+    [request setValue:@"BrowseGithub (github => https://github.com/yaakaito , email => yaakaito@gmail.com , web => http://yaakaito.org)" forHTTPHeaderField:@"User-Agent"];
     AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request
                                                                                         success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
                                                                                             [self setValuesFromJSON:JSON];
