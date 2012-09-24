065faa8cdd146a5437fb87dbcd3295d5abb7b6b3
KAZUMA Ukyo <yaakaito@gmail.com>
2012-09-24 18:49:31 +0900

    いらないコード消したり

--- BrowseGithub/BGGlobalMenuController.m
+++ BrowseGithub/BGGlobalMenuController.m
@@ -29,22 +29,9 @@
 {
     [super viewDidLoad];
 
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-    
     self.tableView.backgroundColor = [UIColor githubGlobalMenuBackgroundColor];
     self.tableView.separatorColor = [UIColor clearColor];
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-}
-
-- (void)viewDidUnload
-{
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
