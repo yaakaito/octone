bd9fbf4480b576b5cbca6426c1116d07388dffa6
KAZUMA Ukyo <yaakaito@gmail.com>
2012-09-25 18:24:08 +0900

    commit comment eventの実装

--- BrowseGithub/BGEventMessageFormatter.m
+++ BrowseGithub/BGEventMessageFormatter.m
@@ -7,16 +7,47 @@
 //
 
 #import "BGEventMessageFormatter.h"
+#import "BGEvent.h"
+#import "UIColor+BrowseGithub.h"
 
 @implementation BGEventMessageFormatter
 
 + (NSAttributedString *)messageWithEvent:(BGEvent *)event {
     
+    if ([event.typeString isEqualToString:@"CommitCommentEvent"]) {
+        return [self commitCommentEventMessageWithEvent:event];
+    }
     return nil;
+}
+
++ (NSAttributedString *)attributedActor:(NSString *)actor {
+    
+    return [[NSAttributedString alloc] initWithString:actor attributes:@{NSForegroundColorAttributeName : [UIColor githubPrimaryColor]}];
+}
+
++ (NSAttributedString *)attributedRepository:(NSString*)repository {
+    
+    return [[NSAttributedString alloc] initWithString:repository attributes:@{NSForegroundColorAttributeName : [UIColor githubPrimaryColor]}];
+}
+
++ (NSAttributedString *)commitCommentEventMessageWithEvent:(BGEvent *)event {
+    
+    NSString *shortHash = [event.payload[@"comment"][@"commit_id"] substringToIndex:9];
+    NSAttributedString *attributedHash = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"@%@", shortHash]];
+    NSAttributedString *base = [[NSAttributedString alloc] initWithString:@" commented on commit "];
+    NSMutableAttributedString *message = [[NSMutableAttributedString alloc] initWithAttributedString:[self attributedActor:event.actorLogin]];
+    [message appendAttributedString:base];
+    [message appendAttributedString:[self attributedRepository:event.repositoryName]];
+    [message appendAttributedString:attributedHash];
+    
+    return message;
 }
 
 + (NSString *)descriptionWithEvent:(BGEvent *)event {
     
+    if ([event.typeString isEqualToString:@"CommitCommentEvent"]) {
+        return event.payload[@"comment"][@"body"];
+    }
     return nil;
 }
 
